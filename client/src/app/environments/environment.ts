4b00b855f08fab5ced8179ad1b7d3d885989e1f2
Devendra Venkat Mutyala <mutyala.devendravenkat15@gmail.com>
2024-10-28 13:05:03 +0530

    Update environment.ts

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: process.env["NODE_ENV"] === 'production', // This will be `true` in production
-  apiUrl: process.env["NODE_ENV"] === 'production'
-    ? 'https://task-manager-3lqc.onrender.com/api'
-    : 'http://localhost:5000/api'
+  production: true, // Set this to true if deploying
+  apiUrl: 'https://task-manager-3lqc.onrender.com/api'
 };
